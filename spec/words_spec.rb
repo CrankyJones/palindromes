95736d2691ebd7ec31a2e9060f5777892d782dac
Jo <joannadawnmiller@gmail.com>
2021-02-24 16:56:46 -0500

    pass second test for palidrome_check
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -4,8 +4,10 @@ require 'words'
 describe(Words) do
   describe('#palindrome_check') do
     it('returns false if the input is not palindrome') do
-
       expect(Words.palindrome_check("boat")).to(eq(false))
+    end
+    it('returns true if the input is a palindrome') do
+      expect(Words.palindrome_check("racecar")).to(eq(true))
     end
   end
 end
