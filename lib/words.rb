e66b49fd96315dc1f45833128492254d9f544930
Jo <joannadawnmiller@gmail.com>
2021-02-24 19:54:04 -0500

    create function to check entire file for word palindromes
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- lib/words.rb
+++ lib/words.rb
@@ -1,13 +1,23 @@
 class Words < String
   def self.palindrome_check(string)
-    word = string.split(' ').join.downcase()
-    if word == word.reverse
+    word = string.gsub(/[\W_]/, '').downcase()
+    if word == word.reverse 
+      puts "#{word} is a palindrome."
       true
     else
       false
     end
   end
 
-
-end    
-
+  def self.file_check(file)
+    file_data = File.read(file).split
+    palindrome_counter = 0
+    file_data.each() do |str|
+      if (palindrome_check(str) == true)
+        palindrome_counter += 1
+      end
+    end
+    puts "#{palindrome_counter} total palindromes in the file."
+  end
+  
+end
